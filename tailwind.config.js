b57d4c775696d6e0c4e3b9827f9b69e829d87910
eric <ericmbouwe@yahoo.fr>
2022-08-29 21:06:01 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
         corbel: "'corbel-regular', sans-serif;",
-        caviarDreams: "'corbel-regular', sans-serif;"
+        caviarDreams: "'corbel-regular', sans-serif;",
+        popins: "'Poppins', sans-serif;",
       },
       colors: {
         blue: {
-          dark: '#00c2cb',
-          cyan: '#231f20'
+          dark: "#00c2cb",
+          cyan: "#231f20",
         },
         gray: {
-          base: '#8e8c98'
-        }
-      }
+          base: "#8e8c98",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
